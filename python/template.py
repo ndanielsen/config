967ae81087b742f90161cf796fcd4437df585359
Nathan Danielsen <nathanjdanielsen@gmail.com>
2015-06-21 13:02:40 -0400

    added future python3

--- python/template.py
+++ python/template.py
@@ -17,6 +17,16 @@ Copyright (c) 2015 Nathan Danielsen. All rights reserved.### COPYRIGHT
 import this
 
 
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+from builtins import (
+         bytes, dict, int, list, object, range, str,
+         ascii, chr, hex, input, next, oct, open,
+         pow, round, super,
+         filter, map, zip)
+
+
 ### Third party import (in alphabetical order )
 
 
