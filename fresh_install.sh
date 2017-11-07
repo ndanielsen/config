9a73abb73b8a59c4a77dabb6ee13d6969366fbd3
Nathan <ndanielsen@users.noreply.github.com>
2017-11-07 08:44:59 -0500

    Create fresh_install.sh

---
+++ fresh_install.sh
@@ -0,0 +1,20 @@
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
+
+sudo apt-get install -y postgresql postgresql-contrib lib
+
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+
+pyenv install 3.6.2
+
+sudo apt update
+sudo apt-get update
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt update; sudo apt install atom
+apm install python pigments
+apm install pigments
+apm install tree-view
+apm uninstall tree-vi
+apm install minimap
+apm  install highlight-selected minimap-highlight-selected
+apm install autoclose-html
+apm install auto-detect-indentation atom-beautify
